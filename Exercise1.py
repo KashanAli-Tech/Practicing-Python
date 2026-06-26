6b0bb278f3b8020cf3df7bd1e1bd01c37f9ae293
Kashanali217 <kashan2172009@gmail.com>
2026-06-26 23:37:58 +0100

    renamed file VariableEx.py to Exercise1.py

---
+++ Exercise1.py
@@ -0,0 +1,10 @@
+# from this point on to push something to github use #
+# git add .
+# git commit -m "message"
+# git push
+car1 = "Mercedes"
+car2 = "BMW"
+choice = int(input("enter car number 1 or 2"))
+if choice == 1:
+    car1 = "Audi"
+    print(car1)

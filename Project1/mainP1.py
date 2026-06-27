95a2c783f7283c341041423c7cd19421a01251ff
KashanAli-Tech <off.kashanali@gmail.com>
2026-06-27 14:56:08 +0100

    Added Contacts function to mainP1.py in Project1

--- Project1/mainP1.py
+++ Project1/mainP1.py
@@ -1,3 +1,47 @@
+contacts = dict()
+
+def Contacts(contacts):
+    # contacts = {name : phone_number}
+    print("Welcome to Contacts\n" 
+    "1. Add Contacts\n"
+    "2. Search Contacts\n"
+    "3. Update Contacts\n"
+    "4. Delete Contacts\n")
+    contacts_menu = int(input("Please enter an integer between 1-4: "))
+
+    match contacts_menu:
+        case 1:
+            newcontact_name = input("Enter the name of the Contact: ")
+            newcontact_number = input("Enter the phone number of the Contact: ")
+            contacts.update({newcontact_name:newcontact_number})
+            print("Contact Added Successfully")
+            
+        case 2:
+            newcontact_name = input("Enter the name of the Contact: ")
+            if newcontact_name in contacts:
+                print(newcontact_name, ":", contacts[newcontact_name])
+            else:
+                print("Contact not found")
+
+        case 3:
+            newcontact_name = input("Enter the name of the Contact: ")
+            newcontact_number = input("Enter the new phone number of the Contact: ")
+            contacts.update({newcontact_name:newcontact_number})
+            print("Contact Updated Successfully")
+
+        case 4:
+            newcontact_name = input("Enter the name of the Contact: ")
+            contacts.pop(newcontact_name)
+            print("Contact Deleted Successfully")
+            
+        case _:
+            print("Please enter a number between 1-7")
+
+
+
+
+
+
 while True:
     print("Welcome to Life Manager System\n"
       "1. Contacts\n"
@@ -11,8 +55,8 @@ while True:
 
     match menu_input:
         case 1:
-            #call function for Contacts
-            pass
+                Contacts(contacts)
+            
         case 2:
             #call function for Tasks
             pass
@@ -32,3 +76,4 @@ while True:
             break
         case _:
             print("Please enter a number between 1-7")
+

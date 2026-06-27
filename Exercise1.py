e0b8b0253aefa49a7fa7a93815c80ab63e2f8025
KashanAli-Tech <off.kashanali@gmail.com>
2026-06-27 11:26:51 +0100

    Completed Task 2 of Exercise 1

--- Exercise1.py
+++ Exercise1.py
@@ -2,10 +2,13 @@
 # git add .
 # git commit -m "message"
 # git push
+
+#These are just warm up tasks actual exercises with start on Exercise2 and so on
+# 1st Task of Exercsice 1
 #FizzBuzz+
 #Print numbers 1–100
 #Replace multiples of 3 with "Fizz", 5 with "Buzz", 7 with "Pop", combos like "FizzBuzzPop"
-
+"""
 for i in range(1, 101):
     if i%3==0:
         print("Fizz")
@@ -15,3 +18,19 @@ for i in range(1, 101):
         print("Pop")
     else:
         print(i)
+"""
+#Completed
+
+""" Palindrome Checker
+Check if string reads same backward
+Ignore spaces + capital letters """
+
+user_string = input("Enter you string: ")
+if user_string == user_string[::-1]:
+    print("palindrome")
+else:
+    print("Not Palindrome")
+
+
+
+    
